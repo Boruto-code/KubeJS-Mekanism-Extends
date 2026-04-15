083ae55e3beb80cd04189a7780f923c390e57e30
Boruto-code <hamburger_nb_666@outlook.com>
2026-04-15 18:06:09 +0800

    Update kubejs.plugins.txt

--- src/main/java/com/hamburger0abcde/kubejs/mekanism_extends/MekanismExtendsKubeJSPlugin.java
+++ src/main/java/com/hamburger0abcde/kubejs/mekanism_extends/MekanismExtendsKubeJSPlugin.java
@@ -3,7 +3,6 @@ package com.hamburger0abcde.kubejs.mekanism_extends;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
 import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber(modid = KubeJSMekanismExtends.MODID)
 public class MekanismExtendsKubeJSPlugin implements KubeJSPlugin {
 
 }
