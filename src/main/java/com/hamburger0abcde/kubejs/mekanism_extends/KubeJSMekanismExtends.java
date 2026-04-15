bb9ca097ea2c05e034ab1361333172f44126dafd
Boruto-code <hamburger_nb_666@outlook.com>
2026-04-15 17:12:31 +0800

    Update build.gradle

---
+++ src/main/java/com/hamburger0abcde/kubejs/mekanism_extends/KubeJSMekanismExtends.java
@@ -0,0 +1,8 @@
+package com.hamburger0abcde.kubejs.mekanism_extends;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(KubeJSMekanismExtends.MODID)
+public class KubeJSMekanismExtends {
+    public static final String MODID = "kubejs_mekanism_extends";
+}
---
+++ src/main/java/com/hamburger0abcde/kubejs/mekanism_extends/MekanismExtendsKubeJSPlugin.java
@@ -0,0 +1,9 @@
+package com.hamburger0abcde.kubejs.mekanism_extends;
+
+import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = KubeJSMekanismExtends.MODID)
+public class MekanismExtendsKubeJSPlugin implements KubeJSPlugin {
+
+}
